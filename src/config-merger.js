574a14afcca359e34c0b02b6fbe2f24927506e51
Jack Le Hamster <jacklehamster@gmail.com>
2022-01-28 04:04:07 -0800

    Add grid template.

--- src/config-merger.js
+++ src/config-merger.js
@@ -1,10 +1,18 @@
 const { FileUtils } = require("dok-file-utils");
 const math = require('mathjs');
+const format = require("string-template");
+
 
 class ConfigMerger {
 	constructor(fileUtils, constants) {
 		this.fileUtils = fileUtils || new FileUtils();
 		this.constants = constants || {};
+		this.ignoredTags = {
+			template: true,
+			templates: true,
+			repeat: true,
+			table: true,
+		};
 	}
 
 	async process(data, gamePath, gameSettings) {
@@ -55,11 +63,11 @@ class ConfigMerger {
 		return translatedData;
 	}
 
-	async translate(data, gameSettings, index) {
+	async translate(data, gameSettings, index, coordinates) {
 		if (!data) {
 			return data;
 		} else if (Array.isArray(data)) {
-			return Promise.all(data.map(d => this.translate(d, gameSettings, index)));
+			return Promise.all(data.map(d => this.translate(d, gameSettings, index, coordinates)));
 		} else if (typeof(data) === "object") {
 			if (data.IGNORE) {
 				return null;
@@ -67,13 +75,29 @@ class ConfigMerger {
 			if (data.repeat && (typeof index === "undefined")) {
 				return Promise.all(
 					new Array(data.repeat).fill(null)
-					.map((_, index) => this.translate(data, gameSettings, index)));
+						.map((_,index) => index)
+						.map(index => this.translate(data, gameSettings, index, coordinates)));
+			}
+			if (data.table && (typeof coordinates === "undefined")) {
+				const rows = data.table[0] || 1;
+				const cols = data.table[1] || 1;
+				const dims = data.table[2] || 1;
+				const dimensions = [];
+				for (let row = 0; row < rows; row++) {
+					for (let col = 0; col < cols; col++) {
+						for (let dim = 0; dim < dims; dim++) {
+							dimensions.push([col, row, dim]);
+						}
+					}
+				}
+				return Promise.all(
+					dimensions.map(coordinates => this.translate(data, gameSettings, index, coordinates)));
 			}
 			const translatedData = {};
 
 			for (let a in data) {
-				if (a !== "templates" && a !== "template" && a !== "repeat") {
-					translatedData[a] = await this.translate(data[a], gameSettings, index);
+				if (!this.ignoredTags[a]) {
+					translatedData[a] = await this.translate(data[a], gameSettings, index, coordinates);
 				}
 			}
 			return translatedData;
@@ -81,16 +105,36 @@ class ConfigMerger {
 
 		if (typeof(data)==="string") {
 			const group = data.match(/^{([^}]+)}$/);
+			const viewportSize = gameSettings.viewportSize || [0, 0];
 			if (group) {
-				const viewportSize = gameSettings.viewportSize || [0, 0];
 				const value = math.evaluate(group[1], {
 					... this.constants,
 					viewportWidth: viewportSize[0],
 					viewportHeight: viewportSize[1],
 					index: index ?? 0,
 					random: Math.random(),
+					row: coordinates ? coordinates[0] : 0,
+					col: coordinates ? coordinates[1] : 0,
+					dim: coordinates ? coordinates[2] : 0,
 				});
 				return value;
+			} else {
+				const groups = data.match(/{([^}]+)}/g);
+				if (groups) {
+					const values = groups.map(group => math.evaluate(group.match(/^{([^}]+)}$/)[1], {
+						... this.constants,
+						viewportWidth: viewportSize[0],
+						viewportHeight: viewportSize[1],
+						index: index ?? 0,
+						random: Math.random(),
+						row: coordinates ? coordinates[0] : 0,
+						col: coordinates ? coordinates[1] : 0,
+						dim: coordinates ? coordinates[2] : 0,
+					}));
+					return format(data.split(/{[^}]+}/g).map((text, index) => {
+						return `${text}{${index}}`;
+					}).join(""), values.concat(""));
+				}
 			}
 		}
 		return data;
--- src/index.test.js
+++ src/index.test.js
@@ -24,10 +24,6 @@ describe('ConfigMerger', function() {
   		"field1": 123,
   		template: "test-template.json",
   		"field3": "{constant * 10}",
-  		"multiField": {
-  			"repeat": 5,
-  			"value": "{index * 2}"
-  		},
   	};
 
   	const result = await configMerger.process(source, "path/", {
@@ -36,11 +32,50 @@ describe('ConfigMerger', function() {
   	expect(result.field1).to.equal(123);
   	expect(result.field2).to.equal(345);
   	expect(result.field3).to.equal(5670);
-  	expect(result.multiField[0].value).to.equal(0);
-  	expect(result.multiField[1].value).to.equal(2);
-  	expect(result.multiField[2].value).to.equal(4);
-  	expect(result.multiField[3].value).to.equal(6);
-  	expect(result.multiField[4].value).to.equal(8);
   	console.log("result", result);
+  });
+
+  it('should merge with repeat', async function() {
+    const fileUtils = new FileUtils(MockXhr);
+    const configMerger = new ConfigMerger(fileUtils, {
+      constant: 567,
+    });
+
+    const source = {
+      "multiField": {
+        "repeat": 5,
+        "value": "{index * 2}"
+      },
+    };
+
+    const result = await configMerger.process(source, "path/", {
+      viewportSize: [100, 200],
+    });
+    expect(result.multiField[0].value).to.equal(0);
+    expect(result.multiField[1].value).to.equal(2);
+    expect(result.multiField[2].value).to.equal(4);
+    expect(result.multiField[3].value).to.equal(6);
+    expect(result.multiField[4].value).to.equal(8);
+    console.log("result", result);
+  });
+
+  it('should merge with table', async function() {
+    const fileUtils = new FileUtils(MockXhr);
+    const configMerger = new ConfigMerger(fileUtils, {
+      constant: 567,
+    });
+
+    const source = {
+      "multiTable": {
+        "table": [5, 3, 2],
+        "value": "{row} / {col} / {dim}"
+      },
+    };
+
+    const result = await configMerger.process(source, "path/", {
+    });
+    expect(result.multiTable.length).to.equal(5*3*2);
+    expect(result.multiTable[result.multiTable.length - 1].value).to.equal('2 / 4 / 1');
+    console.log("result", result);
   });
 });
